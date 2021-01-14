2a77b0aa5095033fc90a4b8e6a9eb24ec5b0fe5d
chainvisions <67360117+chainvisions@users.noreply.github.com>
2021-01-14 11:19:06 -0700

    bug: Patch SYRUP bug

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -313,6 +313,9 @@ contract MasterChef is Ownable {
     function emergencyWithdraw(uint256 _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
+        if(_pid == 0) {
+            syrup.burn(msg.sender, user.amount);
+        }
         pool.lpToken.safeTransfer(address(msg.sender), user.amount);
         emit EmergencyWithdraw(msg.sender, _pid, user.amount);
         user.amount = 0;
